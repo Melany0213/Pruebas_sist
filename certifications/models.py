4da2187bb6ef1e27eaa5333c9349a39268999b42
Melany0213 <melcr132000@gmail.com>
2023-12-01 15:00:42 -0500

    Nogarantiza que el estudiante aparezca una sola vez, sino todas las veces que estaen solicitud.

--- certifications/models.py
+++ certifications/models.py
@@ -3,7 +3,8 @@ from django.db import models
 from django.forms import ValidationError
 from django.core.validators import MaxValueValidator
 from django.core.exceptions import ValidationError
-from main_app.admin import User
+from django.contrib.auth import get_user_model
+from main_app.models import User
 #from django.utils import timezone
 
 
@@ -17,7 +18,12 @@ class Estudiante(models.Model):
         ("Facultad FTE", "FTE"),
         ("Facultad CITEC", "CITEC"),
     ]
-    #user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+    CARRERA_CHOICES = [
+        ("Ingeniería en Ciencias Informáticas","ICI"),
+        ("Ingeniería en Bioinformática", "BIO"),
+        ("Redes y seguridad informática", "ARSI"),
+        ("Ingeniería en Ciberseguridad", "Ciber"),
+    ]
     year = models.CharField(max_length=4)
     group = models.CharField(max_length=10)
     facultad = models.CharField(choices = FACULTAD_CHOICES, max_length=100, blank=True, null=True)
@@ -26,7 +32,12 @@ class Estudiante(models.Model):
     last_name = models.CharField(max_length=35)
     second_surname = models.CharField(max_length=35)
     decano = models.CharField(max_length=150)
-    career = models.CharField(max_length=150)
+    career = models.CharField(choices = CARRERA_CHOICES , max_length=100)
+
+    @property
+    def ultima_solicitud(self):
+        solicitud = Solicitud.objects.filter(estudiante=self).order_by('-date_has_solicitud')
+        return solicitud[0] if solicitud.exists() else None
 
 
 
@@ -35,15 +46,18 @@ class Profesor(models.Model):
     facultad = models.PositiveIntegerField(default=0)
 
 class Certification(models.Model):
-    date_close_sol = models.DateField()#Comienza la certificacion 1 dia despues
-    date_certification = models.DateField()
-    date_speaking = models.DateField()
-    date_writing_reading_listening = models.DateField()
+    date_close_sol = models.DateField(unique=True)#Comienza la certificacion 1 dia despues
+    date_certification = models.DateField(unique=True)
+    date_speaking = models.DateField(unique=True)
+    date_writing_reading_listening = models.DateField(unique=True)
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-class Acta(models.Model):
+class Solicitud(models.Model):
+    estudiante = models.ForeignKey("certifications.Estudiante", on_delete=models.CASCADE)
+    certification = models.ForeignKey("certifications.Certification", on_delete=models.CASCADE)
+    date_has_solicitud = models.DateField(auto_now=True)
     NOTES_CHOICES = [
         ("Basic  1","A1"),
         ("Basic  2", "A2"),
@@ -51,26 +65,22 @@ class Acta(models.Model):
         ("Upper Intermediate", "B2"),
         ("Advanced", "C1"),
     ]
-    reading = models.CharField( choices = NOTES_CHOICES, max_length=20)
-    listening = models.CharField( choices = NOTES_CHOICES, max_length=20)
-    writing = models.CharField( choices = NOTES_CHOICES, max_length=20)
-    speaking = models.CharField( choices = NOTES_CHOICES, max_length=20)
-    equivalent = models.CharField(max_length=35)
-
-class Solicitud(models.Model):
-    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE, default=0)
-    certification = models.ForeignKey(Certification, on_delete=models.CASCADE, default=0)
-    #acta = models.ManyToManyField( Acta, blank=True)
-    date_has_solicitud = models.DateField(auto_now=True)
+    acta_reading = models.CharField( choices = NOTES_CHOICES, max_length=20, default='')
+    acta_listening = models.CharField( choices = NOTES_CHOICES, max_length=20, default='')
+    acta_writing = models.CharField( choices = NOTES_CHOICES, max_length=20, default='')
+    acta_speaking = models.CharField( choices = NOTES_CHOICES, max_length=20, default='')
+    acta_equivalent = models.CharField(max_length=35, default='')
 
     #Guardo la fecha de la solicitud con la fecha actual del sistema
     def clean(self):
         if self.date_has_solicitud >= self.certification.date_close_sol:
             raise ValidationError("La solicitud realizada debe ser hecha antes de la fecha de cierre de las mismas")
 
+
 class Jurado(models.Model):
+    certification = models.ForeignKey(Certification, on_delete=models.CASCADE)
     TIPO_TRIBUNAL_CHOICES = [
-        ("Examenes escritos",  "Exámenes de las 4 habilidades"),
+        ("Examenes escritos",  "Exámenes de las 3 habilidades"),
         ("Examen oral",  "Examen oral (speaking)"),
         ("Calificación de escucha", "Calificación de escucha (listening)"),
         ("Calificación de escritura", "Calificación de escritura (writing)"),
@@ -92,16 +102,6 @@ class Jurado(models.Model):
             if(coincidencias):
                 raise ValidationError(
                 "Alguno de los profesores elegidos ya pertenece a un tribunal del mismo tipo" )
-        # # Verifica que solo se elija un profesor como jefe de tribunal
-        # if self.jefe_tribunal and self.jefe_tribunal not in self.profesores.all():
-        #     raise ValidationError(
-        #         "El profesor seleccionado como jefe de tribunal debe estar en la lista de profesores del tribunal." )
-
-    # def save(self, *args, **kwargs):
-    #     # Verifica si el tribunal tiene solo un profesor y asignar None a jefe_tribunal si es así
-    #     if self.profesores.count() == 1:
-    #             self.jefe_tribunal = None
-    #     super().save(*args, **kwargs)
 
 class Folio(models.Model):
     tomo = models.IntegerField(validators=[MaxValueValidator(100)])
@@ -109,7 +109,7 @@ class Folio(models.Model):
 
 class Hago_Constar(models.Model):
     estudiante = models.OneToOneField(
-        Estudiante, on_delete=models.CASCADE, default=0)
+        Estudiante, on_delete=models.CASCADE)
     emision = models.DateField(auto_now=True)
     folio = models.ForeignKey(Folio, on_delete=models.CASCADE, null=True)
 
